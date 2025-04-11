a0575323f594b0f91db980824805b9e6a1cdd5d1
stphalstaro <sfiguei5@my.centennialcollege.ca>
2025-04-11 13:15:10 -0400

    Fix: review submission with user and schema update

--- filament-vault-backend/models/Filament.js
+++ filament-vault-backend/models/Filament.js
@@ -9,10 +9,18 @@ const FilamentSchema = new mongoose.Schema(
     available_colours: { type: Array },
     printing_temperature: { type: String },
     enclosure_required: { type: String },
+    purchase_from: { type: String },
+    reviews: [
+      {
+        review: { type: String, required: true },
+        rating: { type: Number, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        timestamp: { type: Date, default: Date.now }
+      }
+    ],
     created_at: { type: Date, default: Date.now },
-    purchase_from: {type: String},
   },
-  { collection: "Filament Info" } 
+  { collection: "Filament Info" }
 );
 
 module.exports = mongoose.model("Filament", FilamentSchema, "Filament Info");
